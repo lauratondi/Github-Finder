cc9a4b7241beddf38f575251c52474d3d5ca5105
Lauramars <lauramars85@gmail.com>
2020-04-23 14:27:22 +0200

    directory build for deployment

---
+++ build/precache-manifest.0b639fb2275bee8b1ab7312f30cece1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd688566dcdabeac3df70bb28cc70f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cce6776529c2e359765f",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "2c7321c76ac1f5e1cda0",
+    "url": "/static/js/2.e5e3150c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5e3150c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce6776529c2e359765f",
+    "url": "/static/js/main.5eb0d756.chunk.js"
+  },
+  {
+    "revision": "64703a15ab18469962d9",
+    "url": "/static/js/runtime-main.1ae1be18.js"
+  }
+]);
